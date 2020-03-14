f10fa04937552d3f2df57c5fd383f44747940dcb
psyfer <praneshnegi@gmail.com>
2020-03-14 23:58:26 +0530

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,8 +1,11 @@
 var mob = /iPhone|iPad|Android/i.test(navigator.userAgent);
 var elem = document.getElementById('app-web-head');
+var elem1 = document.getElementById('visit-project');
 if(mob){
   elem.innerHTML = "Download the Application";
+  elem1.href = "https://gofile.io/?c=oXHruH";
 }
 else{
   elem.innerHTML = "Check Out the Application";
+  elem1.href= "https://finlytics.000webhostapp.com/";
 }
