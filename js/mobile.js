34e9772d4d601cbce2e246192b3accf4cc798372
psyfer <praneshnegi@gmail.com>
2020-03-15 00:34:48 +0530

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,11 +1,12 @@
 var mob = /iPhone|iPad|Android/i.test(navigator.userAgent);
 var elem = document.getElementById('app-web-head');
 var elem1 = document.getElementById('visit-project');
+var elem2 = document.getElementById('project-icon');
 if(mob){
   elem.innerHTML = "Download the Application";
 }
 else{
   elem.innerHTML = "Check Out the Website!";
   elem1.href= "https://finlytics.000webhostapp.com/";
-  window.open(elem1.href,"_blank");
+  elem2.className = "fa fa-search img-fluid";
 }
