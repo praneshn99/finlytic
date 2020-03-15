baa81d4ba656be116bc3ea4f070d8cf3c134e79a
psyfer <praneshnegi@gmail.com>
2020-03-15 12:23:15 +0530

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,8 +1,10 @@
 var mob = /iPhone|iPad|Android/i.test(navigator.userAgent);
 var elem = document.getElementById('app-web-head');
 var elem1 = document.getElementById('visit-project');
+var elem2 = document.getElementById('web-mobile');
 if(mob){
   elem.innerHTML = "Download the Application";
+  elem2.innerHTML = "To visit the <strong>website</strong> instead of downloading the application, <a href="https://finlytics.000webhostapp.com/" target="_blank">Click Here</a>";
 }
 else{
   elem.innerHTML = "Check Out the Website";
