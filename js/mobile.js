d156784ba4098c1c79a6dd16f887c52cb1059f4b
psyfer <praneshnegi@gmail.com>
2020-03-15 12:26:30 +0530

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -3,9 +3,7 @@ var elem = document.getElementById('app-web-head');
 var elem1 = document.getElementById('visit-project');
 var elem2 = document.getElementById('web-mobile');
 if(mob){
-  elem.innerHTML = "Download the Application";
-  elem2.innerHTML = "To visit the <strong>website</strong> instead of downloading the application, <a href="https://finlytics.000webhostapp.com/" target="_blank">Click Here</a>";
-}
+  elem.innerHTML = "Download the Application";}
 else{
   elem.innerHTML = "Check Out the Website";
   elem1.href= "https://finlytics.000webhostapp.com/";
